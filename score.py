768a1e2141a8ddc93f5a6b02fa0ec2e18031bdb8
chappidiSiddhartha <51066018+chappidiSiddhartha@users.noreply.github.com>
2024-12-23 11:50:22 -0500

    Update score.py

--- score.py
+++ score.py
@@ -8,7 +8,7 @@ import json
 
 # Azure ML Setup
 workspace_name = "my-ml-workspace3"
-subscription_id = "a22eeea6-98d6-4951-a80c-326264b6750f"
+subscription_id = "#######-####-####-###-###########"
 resource_group = "my-ml-resource-group3"
 
 # Global Variables
